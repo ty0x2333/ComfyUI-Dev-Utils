Fix the VRAM calculation bug for the ExecutionTime Node.

nodes/execution_time.py
@@ -1,8 +1,11 @@
 import time
 
+import torch
+
 import execution
 import server
-import model_management
+# import model_management
+
 
 class ExecutionTime:
     CATEGORY = "TyDev-Utils/Debug"
@@ -21,29 +24,46 @@ class ExecutionTime:
 
 CURRENT_START_EXECUTION_DATA = None
 
-def get_free_vram():
-    dev = model_management.get_torch_device()
-    if hasattr(dev, 'type') and (dev.type == 'cpu' or dev.type == 'mps'):
+
+# def get_free_vram():
+#     dev = model_management.get_torch_device()
+#     if hasattr(dev, 'type') and (dev.type == 'cpu' or dev.type == 'mps'):
+#         return 0
+#     else:
+#         return model_management.get_free_memory(dev)
+
+
+def get_peak_memory():
+    if not torch.cuda.is_available():
         return 0
-    else:
-        return model_management.get_free_memory(dev)
+    device = torch.device('cuda')
+    return torch.cuda.max_memory_allocated(device)
+
+
+def reset_peak_memory_record():
+    if not torch.cuda.is_available():
+        return
+    device = torch.device('cuda')
+    torch.cuda.reset_max_memory_allocated(device)
+
 
 def handle_execute(class_type, last_node_id, prompt_id, server, unique_id):
     if not CURRENT_START_EXECUTION_DATA:
         return
     start_time = CURRENT_START_EXECUTION_DATA['nodes_start_perf_time'].get(unique_id)
-    start_vram = CURRENT_START_EXECUTION_DATA['nodes_start_vram_time'].get(unique_id)
+    start_vram = CURRENT_START_EXECUTION_DATA['nodes_start_vram'].get(unique_id)
     if start_time:
         end_time = time.perf_counter()
-        end_vram = get_free_vram()
         execution_time = end_time - start_time
-        # use abs because it can be negative, that means the model_manager already loaded the model when we started profiling and cleared the model at the end
-        # it's not super accurate for anything small but for large models it's really handy to know what the footprint is
-        vram_used = abs(end_vram - start_vram)
+
+        end_vram = get_peak_memory()
+        vram_used = end_vram - start_vram
+        print(f"end_vram - start_vram: {end_vram} - {start_vram} = {vram_used}")
         if server.client_id is not None and last_node_id != server.last_node_id:
             server.send_sync(
                 "TyDev-Utils.ExecutionTime.executed",
-                {"node": unique_id, "prompt_id": prompt_id, "execution_time": int(execution_time * 1000), "vram_used": vram_used  },
+                {"node": unique_id, "prompt_id": prompt_id, "execution_time": int(execution_time * 1000),
+                 "vram_used": vram_used},
                 server.client_id
             )
         print(f"#{unique_id} [{class_type}]: {execution_time:.2f}s - vram {vram_used}b")
@@ -103,7 +123,7 @@ def swizzle_send_sync(self, event, data, sid=None):
         CURRENT_START_EXECUTION_DATA = dict(
             start_perf_time=time.perf_counter(),
             nodes_start_perf_time={},
-            nodes_start_vram_time={}
+            nodes_start_vram={}
         )
 
     origin_func(self, event=event, data=data, sid=sid)
@@ -125,7 +145,8 @@ def swizzle_send_sync(self, event, data, sid=None):
         else:
             node_id = data.get("node")
             CURRENT_START_EXECUTION_DATA['nodes_start_perf_time'][node_id] = time.perf_counter()
-            CURRENT_START_EXECUTION_DATA['nodes_start_vram_time'][node_id] = get_free_vram()
+            reset_peak_memory_record()
+            CURRENT_START_EXECUTION_DATA['nodes_start_vram'][node_id] = get_peak_memory()
 
 
 server.PromptServer.send_sync = swizzle_send_sync